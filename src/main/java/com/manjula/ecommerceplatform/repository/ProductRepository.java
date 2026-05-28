ede82b88bb6bb2d69677c9829899a70e6c3c99c0
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-05-28 12:10:50 +0200

    mod ProductRepository.java
    started adding queries

--- src/main/java/com/manjula/ecommerceplatform/repository/ProductRepository.java
+++ src/main/java/com/manjula/ecommerceplatform/repository/ProductRepository.java
@@ -3,8 +3,11 @@ package com.manjula.ecommerceplatform.repository;
 import com.manjula.ecommerceplatform.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     //Required Queries:
-
+    List<Product> findByCategoryContaining(String category);
+    List<Product> findBy
 }
