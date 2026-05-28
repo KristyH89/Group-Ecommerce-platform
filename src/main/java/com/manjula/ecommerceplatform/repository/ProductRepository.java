549c9f3dd5169a9829ffdaa690c51ece6e9a1afe
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-05-28 12:25:40 +0200

    mod ProductRepository.java
    finished required queries

--- src/main/java/com/manjula/ecommerceplatform/repository/ProductRepository.java
+++ src/main/java/com/manjula/ecommerceplatform/repository/ProductRepository.java
@@ -3,11 +3,12 @@ package com.manjula.ecommerceplatform.repository;
 import com.manjula.ecommerceplatform.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     //Required Queries:
     List<Product> findByCategoryContaining(String category);
-    List<Product> findBy
+    List<Product> findByPriceBetween(BigDecimal priceStart, BigDecimal priceEnd);
 }
