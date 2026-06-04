a5e67baddcf8a0a5c07d1646cd24debe310da3e3
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-06-04 10:27:50 +0200

    mod Product DTO files
    add fields

--- src/main/java/com/manjula/ecommerceplatform/dto/ProductRequestDTO.java
+++ src/main/java/com/manjula/ecommerceplatform/dto/ProductRequestDTO.java
@@ -1,14 +1,10 @@
 package com.manjula.ecommerceplatform.dto;
 
-import com.manjula.ecommerceplatform.entity.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Set;
 
 public record ProductRequestDTO(
 
@@ -16,16 +12,10 @@ public record ProductRequestDTO(
         @Size(max = 200, message  = "Name must not exceed 200 characters")
         String name,
 
-        @NotNull(message  = "imageUrls cannot be Null")
-        List<String> imageUrls,
-
-        @NotEmpty(message = "Price cannot be blank")
+        @NotEmpty(message = "Price cannot be Empty")
         BigDecimal price,
 
-        @NotNull(message = "Category cannot be Null")
-        Category category,
-
-        @NotNull(message = "Promotions cannot be Null")
-        Set<Long> promotions
+        @NotEmpty(message = "Category cannot be Empty")
+        Long categoryId
 ) {
 }
